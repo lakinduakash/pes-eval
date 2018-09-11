d534372b19a7441459533ce459d2c19b2257fd9e
lakinduakash <lakinduakash@gmail.com>
2018-09-12 01:17:52 +0530

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoginModule} from './login/login.module';
+import {SignupModule} from './signup/signup.module';
+import {EvalFormModule} from './eval-form/eval-form.module';
+import {EvalListModule} from './eval-list/eval-list.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    SignupModule,
+    EvalFormModule,
+    EvalListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
