f7e31622b3b894050a9b6e29a04fca8b22c25ba5
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-09-30 16:33:42 +0530

    evalutorList component

---
+++ src/app/evaluator/evaluator-list/evaluator-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluator-list',
+  templateUrl: './evaluator-list.component.html',
+  styleUrls: ['./evaluator-list.component.css']
+})
+export class EvaluatorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/evaluator/evaluator.module.ts
+++ src/app/evaluator/evaluator.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { EvaluatorListComponent } from './evaluator-list/evaluator-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginComponent, SignupComponent]
+  declarations: [LoginComponent, SignupComponent, EvaluatorListComponent]
 })
 export class EvaluatorModule { }
