08e7e55e6d7f1319413e0e36707a699867ac9fdc
lakinduakash <lakinduakash@gmail.com>
2018-09-30 15:18:42 +0530

    add firbase config pes-eval

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AuthModule} from './core/auth/auth.module';
+import {AngularFireModule} from '@angular/fire';
+import {fireBaseConfig} from '../environments/environment';
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import {AuthModule} from './core/auth/auth.module';
   ],
   imports: [
     BrowserModule,
-    AuthModule
+    AuthModule,
+    AngularFireModule.initializeApp(fireBaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
