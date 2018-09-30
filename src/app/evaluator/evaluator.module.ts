3c82265fac711fc1ee081abc560c35157aad28d7
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-09-30 16:32:37 +0530

    evalutor signup component

--- src/app/evaluator/evaluator.module.ts
+++ src/app/evaluator/evaluator.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, SignupComponent]
 })
 export class EvaluatorModule { }
