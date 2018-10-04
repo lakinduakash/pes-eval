89d129d4e96183a77b9179403c736e036a411f19
lakinduakash <lakinduakash@gmail.com>
2018-10-04 21:33:53 +0530

    add routes

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './eval-signin-up/login/login.component';
+import {SignupComponent} from './eval-signin-up/signup/signup.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports:[RouterModule]
 })
-export class RouteModule { }
+export class AppRouteModule { }
