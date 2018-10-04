d3abb4c2e89c144d75656fce6f6dd79dcf0b6693
lakinduakash <lakinduakash@gmail.com>
2018-10-04 21:56:27 +0530

    import modules

--- src/app/eval-signin-up/eval-signin-up.module.ts
+++ src/app/eval-signin-up/eval-signin-up.module.ts
@@ -2,11 +2,23 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login/login.component';
 import {SignupComponent} from './signup/signup.component';
-import {MatCardModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatProgressBarModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    RouterModule
   ],
   declarations: [LoginComponent, SignupComponent]
 })
