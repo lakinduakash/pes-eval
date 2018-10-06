cad1482b912b5083ec6fd89ff4519083fb5c3bcf
lakinduakash <lakinduakash@gmail.com>
2018-10-06 22:27:59 +0530

    add lazy loaded module for presentation list module

--- src/app/list-presentation/list-presentation.module.ts
+++ src/app/list-presentation/list-presentation.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ListMainComponent} from './list-main/list-main.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const routes:Routes=[
+  {path:'',component:ListMainComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [ListMainComponent]
 })
--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -4,10 +4,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './eval-signin-up/login/login.component';
 import {SignupComponent} from './eval-signin-up/signup/signup.component';
 
+
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/presentations', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'presentations', loadChildren: './list-presentation/list-presentation.module#ListPresentationModule'}
   ];
 
 @NgModule({
