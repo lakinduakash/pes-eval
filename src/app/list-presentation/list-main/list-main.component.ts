c7d4ff17d7ca6ce3cefbea243e541966926d9cd5
lakinduakash <lakinduakash@gmail.com>
2018-10-06 22:17:36 +0530

    add new component for list-presentation

---
+++ src/app/list-presentation/list-main/list-main.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-list-main',
+  templateUrl: './list-main.component.html',
+  styleUrls: ['./list-main.component.css']
+})
+export class ListMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
