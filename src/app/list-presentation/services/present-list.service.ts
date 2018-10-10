6303512c69c7fb4782c086fc6a90a113ffc402ec
lakinduakash <lakinduakash@gmail.com>
2018-10-11 04:34:04 +0530

    clean functions

--- src/app/list-presentation/services/present-list.service.ts
+++ src/app/list-presentation/services/present-list.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {PesFireStoreProviderService} from '../../shared/services/pes-firestore-provider.service';
 import {AuthService} from '../../core/auth/auth.service';
 import {Observable, Subject} from 'rxjs';
-import {Action, DocumentSnapshot} from '@angular/fire/firestore';
+import {DocumentSnapshot} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +13,19 @@ export class PresentListService {
 
   getPresentationList()
   {
-    let s:Subject<Action<DocumentSnapshot<any>>> =new Subject<Action<DocumentSnapshot<any>>>();
+    let s:Subject<any> =new Subject<any>();
     this.auth.user.subscribe(
       user=>{
-        if(user !=null)
-          this.pfp.collection(`usersE`).doc(user.uid).snapshotChanges().subscribe(
-            next=> s.next(next)
+        console.log(user);
+        if(user !=null) {
+          this.pfp.collection(`usersE`).doc(user.uid).get().subscribe(
+            next => s.next(next)
           )
+        }
       }
     );
 
-    return s as Observable<Action<DocumentSnapshot<any>>>
+    return s as Observable<DocumentSnapshot<any>>
 
   }
 
