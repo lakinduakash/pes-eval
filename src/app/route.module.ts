3c77640e6cc33ba6144dbe2c8df24e6ffb1b5acd
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-10-24 10:46:25 +0530

    add list presentation module

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './eval-signin-up/login/login.component';
 import {SignupComponent} from './eval-signin-up/signup/signup.component';
-
+// import {ListPresentationModule} from './list-presentation/list-presentation.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/presentations', pathMatch: 'full'},
