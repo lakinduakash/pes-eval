62e458d9140245d8db49d19a208dcc651f429aed
lakinduakash <lakinduakash@gmail.com>
2018-11-08 00:23:27 +0530

    calculate scroll

--- src/app/eval-form-parser/form-view.component.ts
+++ src/app/eval-form-parser/form-view.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {NavBarTitleService} from '../components/services/nav-bar-title.service';
 import {FormService} from '../shared/services/form.service';
 import {FormModel, Section} from '../shared/model/form-model';
 import {FormEditEventService} from './form-edit-event.service';
+import {RealTimeMarkingService} from '../shared/services/real-time-marking.service';
 
 
 @Component({
@@ -15,8 +16,35 @@ export class FormViewComponent implements OnInit {
 
   @Input('eval-form')evalForm;
 
-  constructor(private formService: FormService, private route: ActivatedRoute, private router: Router,
-              private titleBar: NavBarTitleService,private editEvent:FormEditEventService) {
+  hideBottom=false;
+  hideTop=true;
+
+  @HostListener('window:scroll', ['$event'])
+  doSomething(event) {
+    let windowHeight = "innerHeight" in window ? window.innerHeight
+      : document.documentElement.offsetHeight;
+    let body = document.body, html = document.documentElement;
+    let docHeight = Math.max(body.scrollHeight,
+      body.offsetHeight, html.clientHeight,
+      html.scrollHeight, html.offsetHeight);
+    let windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      this.hideBottom=true;
+      this.hideTop=false
+    }
+    else
+    {
+      this.hideBottom=false;
+      this.hideTop=true
+    }
+
+  }
+
+  constructor(private formService: FormService,
+              private route: ActivatedRoute, private router: Router,
+              private titleBar: NavBarTitleService,
+              private editEvent:FormEditEventService,
+              public realTimeMarking:RealTimeMarkingService) {
   }
 
   form:FormModel;
@@ -27,11 +55,19 @@ export class FormViewComponent implements OnInit {
 
   private routeId: string;
 
+  uid;
+  formId;
+  projectId;
+  presentId;
+
   ngOnInit() {
 
     this.route.params.subscribe(
       params=>{
-        console.log(params);
+        this.uid=params['uid'];
+        this.formId=params['form'];
+        this.projectId=params['p'];
+        this.presentId=params['pr'];
         this.formService.getForm(params['uid'],params['form'],params['p'],params['pr']).subscribe(next => {
           this.form = next.data() as FormModel;
           this.printForm()
@@ -42,6 +78,17 @@ export class FormViewComponent implements OnInit {
     this.editEvent.event.subscribe(next=>console.log(this.form))
 
   }
+
+  init(uid,formid,pid,preid)
+  {
+      this.realTimeMarking.getCurrentStates(uid,pid,preid).subscribe(
+        next=>
+        {
+
+        }
+      )
+  }
+
 
   printForm()
   {
