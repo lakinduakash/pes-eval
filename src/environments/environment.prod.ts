329f1f1977616dd216d761b7a0d29c0fa73a1fcc
lakinduakash <lakinduakash@gmail.com>
2019-01-04 03:22:10 +0530

    Add production keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
   production: true
 };
+
+export const fireBaseConfig = {
+  apiKey: 'AIzaSyA3IEmXc922u6k1E9Qp3URx56pa55BuFZs',
+  authDomain: 'pes-eval.firebaseapp.com',
+  databaseURL: 'https://pes-eval.firebaseio.com',
+  projectId: 'pes-eval',
+  storageBucket: 'pes-eval.appspot.com',
+  messagingSenderId: '721843620769'
+};
+
+export const firBaseConfigPes = {
+  apiKey: 'AIzaSyCmovsqRyc1JWkCh7K8e_SGm7KdFpTIdJY',
+  authDomain: 'my-first-firebase-1319e.firebaseapp.com',
+  databaseURL: 'https://my-first-firebase-1319e.firebaseio.com',
+  projectId: 'my-first-firebase-1319e',
+  storageBucket: '',
+  messagingSenderId: '310316367899'
+};
